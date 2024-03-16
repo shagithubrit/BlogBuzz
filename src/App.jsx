d20eed6e83b5c06b1e1f7c642b5928915c5ce0d1
Shashwat Kapoor <shashwatquora2000@gmail.com>
2024-03-16 06:34:59 +0530

    starter project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+// import { M } from "vite/dist/node/types.d-AKzkD8vd";
+import Post from "./components/Post";
+function App() {
+  return (
+    <main>
+      <Post author=" Maximillian " body="ReactJs is awesome !!"  />
+      <Post author=" Manuel "  body="ReactJs is awesome !!" />
+    </main>
+  );
+}
+
+export default App;
